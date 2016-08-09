f89e910c612323793de2f2f36c167f1ad3b51d4c
Jaśko <jan.palucki@gmail.com>
2016-08-09 20:47:39 +0200

    Skype window with manually created icon and design

---
+++ Skype/Skype.py
@@ -0,0 +1,32 @@
+import tkinter
+
+number = input('How many contacts you want to add ? >>> ')
+contacts = []
+
+for i in range(int(number)):
+    contact = input(str(i+1)+'. contact\'s name: ')
+    contacts += [contact]
+
+window = tkinter.Tk()
+window.title('Skype')
+window.geometry('300x400')
+window.configure(background='sky blue')
+window.wm_iconbitmap('skype.ico')
+
+job = tkinter.Label(window, text='\nStaying lazy...',fg='white',bg='sky blue',font=('Helvetica',14))
+
+def call(name):
+    job.configure(text='\nCalling '+name+'...')
+
+intro = tkinter.Label(window, text='Click a button to call someone.',fg='white',bg='sky blue', font=('Helvetica',15))
+intro.pack()
+
+for con in contacts:
+    con_lab = tkinter.Label(window, text='\nClick to phone '+con,fg='white',bg='sky blue')
+    con_lab.pack()
+    con_but = tkinter.Button(window,text='Call '+con, command=lambda cur_contact = con: call(cur_contact),fg='sky blue',bg='white')
+    con_but.pack()
+
+job.pack()
+
+
