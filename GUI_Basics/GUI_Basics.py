b6b5e9985446e8ae0a3ae2316862850219211a9b
Jaśko <jan.palucki@gmail.com>
2016-08-23 19:48:38 +0200

    Added password wrapping

--- GUI_Basics/GUI_Basics.py
+++ GUI_Basics/GUI_Basics.py
@@ -1,5 +1,12 @@
 import tkinter
 
+def clear():
+    login = ent_login.get()
+    passwd = ent_pass.get()
+    ent_login.delete(0, 'end')
+    ent_pass.delete(0, 'end')
+    lbl_info.pack()
+
 window = tkinter.Tk()
 
 window.title('My First GUI Interface')
@@ -10,11 +17,12 @@ window.configure(background='medium sea green')
 lbl = tkinter.Label(window, text= "Zwróć uwagę na ikonkę...\n",bg='medium sea green', font=('Helvetica', 16))
 lbl_login = tkinter.Label(window, text = 'Login:', bg='medium sea green')
 lbl_pass = tkinter.Label(window, text = 'Password:', bg='medium sea green')
+lbl_info = tkinter.Label(window, text = '\nSuccessfully logged in!', bg='medium sea green')
 
 ent_login = tkinter.Entry(window)
-ent_pass = tkinter.Entry(window)
+ent_pass = tkinter.Entry(window, show="*")
 
-btn = tkinter.Button(window, text = "Log in.",fg='medium sea green',bg='dark slate gray')
+btn = tkinter.Button(window, text = "Log in.",fg='medium sea green',bg='dark slate gray', command = lambda: clear())
 
 #in pack() function parameter fill=tkinter.X will make widget fills the axis in window)
 #parameter side=tkinter.LEFT will whereas put the widgets next to each other starting from left side)
@@ -27,4 +35,5 @@ ent_pass.pack()
 btn.pack()
 
 window.mainloop()
+    
  
