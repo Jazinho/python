658ac93169fe40f6a70aafd86e34a76257886f19
Jaśko <jan.palucki@gmail.com>
2016-09-05 23:06:14 +0200

    Created Question class

---
+++ MusicQuiz/Question.py
@@ -0,0 +1,6 @@
+class Question:
+    path = "probes/"
+    def __init__(self, level, title, bad_answers):
+         self.level = level
+         self.title = title
+         self.bad_answers = bad_answers
