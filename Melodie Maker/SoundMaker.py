fb96364a790988b5ba8f41b4d37fb68bf1096ee6
Jaśko <jan.palucki@gmail.com>
2016-09-06 18:37:36 +0200

    Extracting frequencies outside the file, replacing multiple ifs with one

--- Melodie Maker/SoundMaker.py
+++ Melodie Maker/SoundMaker.py
@@ -1,47 +1,24 @@
 import winsound
 import tkinter
 from PIL import Image,ImageTk
-
-sounds_freq = { 'c':523,
-                'c#':554,
-                'd':587,
-                'd#':622,
-                'e':659,
-                'f':698,
-                'f#':739,
-                'g':783,
-                'g#':830,
-                'a':880,
-                'a#':932,
-                'h':987,
-                'C':1046,
-                'C#':1109,
-                'D':1175,
-                'D#':1245,
-                'E':1319,
-                'F':1397,
-                'F#':1480,
-                'G':1568,
-                'G#':1661,
-                'A':1760,
-                'A#':1865,
-                'H':1976,
-                'CC':2093,
-                'sil':37,
-                }
+from SoundsFrequencies import sounds_freq
+#from PreparedMelodies import melodies
 
 def play():
-    melodies = {"Dla_Elizy":"E200 D#200 E200 D#200 E200 h200 D200 C200 a400 sil100 c200 e200 a200 h400 sil100 e200 g#200 h200 C400 sil200 e200 E200 D#200 E200 D#200 E200 h200 D200 C200 a400 sil100 c200 e200 a200 h400 sil100 e200 C200 h200 a400",
-                "Coco_Jumbo":"D200 F200 A400 sil100 D250 F100 A150 sil150 A300 D300 F200 A300 A#200 A200 G400 sil200 D200 F200 A400 sil100 D250 F100 A150 sil150 A300 G200 F100 G400 F200 sil200 F500 D200 F200 A400 sil100 D250 F100 A150 sil150 A300 D300 F200 A300 A#200 A200 G400 sil200 D200 F200 A400 sil100 D250 F100 A150 sil150 A300 G200 F100 G400 F200 sil200 F500",
-                "Wlazł_Kotek":"G250 sil150 E200 sil150 E250 sil150 F300 sil150 D200 sil150 D250 sil150 C200 E200 G200 sil150 G400 C200 E200 G200 sil150 G400 G250 sil150 E200 sil150 E250 sil150 F300 sil150 D200 sil150 D250 sil150 C200 E200 C200 sil150 C400 C200 E200 C200 sil150 C400",
-                "Oczy_Zielone":"a#400 sil200 g200 a#200 C200 C#400 C200 a#400 C400 g#400 sil800 F400 F#200 F400 sil400 a#400 sil150 g200 a#200 C200 C#400 C200 a#400 C400 g#400 sil800 F400 F#200 F400",
-                "Get_Get_Down":"f400 C100 sil50 C100 sil50 C100 f400 sil200 C100 sil50 C100 sil200 f400 C100 sil50 C100 sil50 C100 f400 sil200 C100 sil50 C100 sil400 F250 F250 G#1500 F250 F250 D#100 F200 sil1200 F250 F250 G#1500 F250 F250 D#100 F200"
-                }
+    melodies = {
+        'Dla_Elizy':"E200 D#200 E200 D#200 E200 h200 D200 C200 a400 sil100 c200 e200 a200 h400 sil100 e200 g#200 h200 C400 sil200 e200 E200 D#200 E200 D#200 E200 h200 D200 C200 a400 sil100 c200 e200 a200 h400 sil100 e200 C200 h200 a400",
+        'Coco_Jumbo':"D200 F200 A400 sil100 D250 F100 A150 sil150 A300 D300 F200 A300 A#200 A200 G400 sil200 D200 F200 A400 sil100 D250 F100 A150 sil150 A300 G200 F100 G400 F200 sil200 F500 D200 F200 A400 sil100 D250 F100 A150 sil150 A300 D300 F200 A300 A#200 A200 G400 sil200 D200 F200 A400 sil100 D250 F100 A150 sil150 A300 G200 F100 G400 F200 sil200 F500",
+        'Wlazł_Kotek':"G250 sil150 E200 sil150 E250 sil150 F300 sil150 D200 sil150 D250 sil150 C200 E200 G200 sil150 G400 C200 E200 G200 sil150 G400 G250 sil150 E200 sil150 E250 sil150 F300 sil150 D200 sil150 D250 sil150 C200 E200 C200 sil150 C400 C200 E200 C200 sil150 C400",
+        'Oczy_Zielone':"a#400 sil200 g200 a#200 C200 C#400 C200 a#400 C400 g#400 sil800 F400 F#200 F400 sil400 a#400 sil150 g200 a#200 C200 C#400 C200 a#400 C400 g#400 sil800 F400 F#200 F400",
+        'Get_Get_Down':"f400 C100 sil50 C100 sil50 C100 f400 sil200 C100 sil50 C100 sil200 f400 C100 sil50 C100 sil50 C100 f400 sil200 C100 sil50 C100 sil400 F250 F250 G#1500 F250 F250 D#100 F200 sil1200 F250 F250 G#1500 F250 F250 D#100 F200"
+        }
     note = ''
     time = ''
-    zapis = str(entry.get())
-    if zapis == "Dla_Elizy":
-        zapis = melodies["Dla_Elizy"]
+    if str(entry.get()) in melodies:
+        zapis = melodies[str(entry.get())]
+        '''
+    if zapis == 'Dla_Elizy':
+        zapis = melodies['Dla_Elizy']
     if zapis == "Coco_Jumbo":
         zapis = melodies["Coco_Jumbo"]
     if zapis == "Wlazł_Kotek":
@@ -50,6 +27,7 @@ def play():
         zapis = melodies["Oczy_Zielone"]
     if zapis == "Get_Get_Down":
         zapis = melodies["Get_Get_Down"]
+        '''
     for char in zapis:
         if char.isdigit():
             time = time + char
