8eb9e91b32cf81ff4e4a1163e3a65149ff776f77
Jaśko <jan.palucki@gmail.com>
2016-09-07 21:14:13 +0200

    Deleted setup() function

--- Hangman/Hangman.py
+++ Hangman/Hangman.py
@@ -87,74 +87,7 @@ def start():
         but.destroy()
         after_game()
 
-def setup():
-    global lvl
-    global guesses
-    global wyswietlane_haslo
-    global slowo
-    global guessed_letters
-    global game_on
-    global step_to_death
-    global choice1
-    global choice2
-    global choice3
-    global window
-    global lines_from_file
-    global rand
-    global lbl
-    global glob_label
-    global but
-    global info
-    global entry
-    global wprow
-    global wprow_lab
-    
-    lvl=0
-    guesses=0
-    wyswietlane_haslo = ''
-    slowo = ''
-    guessed_letters=''
-    game_on = True
-    step_to_death=True
-
-    window = tkinter.Tk()
-    choice1 = tkinter.Button(window, text='Łatwy',command=lambda: set_level(1))
-    choice2 = tkinter.Button(window, text='Średni',command=lambda: set_level(2))
-    choice3 = tkinter.Button(window, text='Trudny',command=lambda: set_level(3))
-    window.geometry('500x500')
-    window.title('Gra Wisielec')
-
-    pom = tkinter.Label(window, text='')
-    pom.pack()
-
-    pom2 = tkinter.Label(window, text='')
-    pom2.pack()
-
-    img = ImageTk.PhotoImage(Image.open("resources/0.png"))
-    glob_label = tkinter.Label(window, image = img)
-    glob_label.pack()
-
-    lbl = tkinter.Label(window, text='Wybierz poziom trudności:\n')
-    lbl.pack()
-
-    choice1.pack()
-    choice2.pack()
-    choice3.pack()
-
-    f=open('Base.txt','r')
-    lines_from_file = f.readlines()
-    random.seed()
-    rand = random.randint(0,3)
-
-    info = tkinter.Label(window, text='\nZgadnij jaka litera znajduje sie w słowie-rozwiązaniu.\nKażda kolejna nietrafiona próba to krok do śmierci wisielca...')
-    info.pack()
-
-    entry = tkinter.Entry(window)
-    but = tkinter.Button(window, text='Check!',command=lambda: start())
-    wprow = 'Sprawdzono: '
-    wprow_lab = tkinter.Label(window, text=wprow)
-
-#global variables and starting program
+#main
 lines_from_file=''
 lvl=0
 guesses=0
@@ -164,6 +97,49 @@ guessed_letters=''
 game_on = True
 step_to_death=True
 
-setup()
+lvl=0
+guesses=0
+wyswietlane_haslo = ''
+slowo = ''
+guessed_letters=''
+game_on = True
+step_to_death=True
+
+window = tkinter.Tk()
+choice1 = tkinter.Button(window, text='Łatwy',command=lambda: set_level(1))
+choice2 = tkinter.Button(window, text='Średni',command=lambda: set_level(2))
+choice3 = tkinter.Button(window, text='Trudny',command=lambda: set_level(3))
+window.geometry('500x500')
+window.title('Gra Wisielec')
+
+pom = tkinter.Label(window, text='')
+pom.pack()
+
+pom2 = tkinter.Label(window, text='')
+pom2.pack()
+
+img = ImageTk.PhotoImage(Image.open("resources/0.png"))
+glob_label = tkinter.Label(window, image = img)
+glob_label.pack()
+
+lbl = tkinter.Label(window, text='Wybierz poziom trudności:\n')
+lbl.pack()
+
+choice1.pack()
+choice2.pack()
+choice3.pack()
+
+f=open('Base.txt','r')
+lines_from_file = f.readlines()
+random.seed()
+rand = random.randint(0,3)
+
+info = tkinter.Label(window, text='\nZgadnij jaka litera znajduje sie w słowie-rozwiązaniu.\nKażda kolejna nietrafiona próba to krok do śmierci wisielca...')
+info.pack()
+
+entry = tkinter.Entry(window)
+but = tkinter.Button(window, text='Check!',command=lambda: start())
+wprow = 'Sprawdzono: '
+wprow_lab = tkinter.Label(window, text=wprow)
 
 window.mainloop()
